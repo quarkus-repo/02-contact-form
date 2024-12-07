5b3b2db2dd737372c08b9dc9dd63dbe389577bc9
AngelTravieso <angeltraviesoc@gmail.com>
2024-12-07 15:32:25 -0400

    Definir el API y crear paso a paso el metodo GET para listar todos los contactos

--- src/main/java/org/acme/ContactResource.java
+++ src/main/java/org/acme/ContactResource.java
@@ -1,4 +1,25 @@
 package org.acme;
 
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+
+import java.util.List;
+
+@Path("/api/contact")
+@Produces("application/json")
+@Consumes("application/json")
 public class ContactResource {
+
+    @Inject
+    EntityManager entityManager;
+
+    @GET
+    public List<Contact> getAllContacts() {
+        return entityManager.createQuery("from Contact", Contact.class).getResultList();
+    }
+
 }
