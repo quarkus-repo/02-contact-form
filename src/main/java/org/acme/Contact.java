f74886b2b750e7c4b0250c5c4a232dd64ed0d27c
AngelTravieso <angeltraviesoc@gmail.com>
2024-12-07 15:27:00 -0400

    Crear y definir la entidad base para persistir los datos con Hibernate ORM y H2

---
+++ src/main/java/org/acme/Contact.java
@@ -0,0 +1,22 @@
+package org.acme;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Contact {
+    @Id
+    @GeneratedValue
+    public Long id;
+
+    public String name;
+    public String email;
+    public boolean dateProtection;
+}
